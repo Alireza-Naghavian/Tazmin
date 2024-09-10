73947458e9241785604787c74bdfdfe303fe3c33
Alireza-Naghavian <160356810+Alireza-Naghavian@users.noreply.github.com>
2024-09-10 12:34:21 +0330

    Update httpServices.js

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -17,4 +17,26 @@ api.interceptors.request.use(
   (res) => res,
   (err) => Promise.reject(err)
 );
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const { data } = await axios.get(`${Base_Url}/user/refresh-token`, {
+          withCredentials: true,
+        });
+
+        return api(originalRequest);
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 export default http;
